4f2fb8931e8a0fed84158c2973d6313975f94086
Gröhn Juho A <gjuho@da5-cs-b221-19.helsinki.fi>
2017-02-08 16:48:20 +0200

    Modasin Main.javaa niin, että localhost:4567 ei viittaa index.html-tiedostoon, vaan etusivu.html-tiedostoon. Loin etusivu.html-tiedoston ja sinne linkit keskustelualueisiin. Muokkasin Main.javaa sen verran, kuin oli tarpeellista. git status

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -32,27 +32,30 @@ public class Main {
             System.out.println(e.getAlueenNimi() + " - " + e.getKuvaus());
            // Tulostaa alueiden nimet + kuvaukset
 
+        //Etusivu: määritellään, että etusivun URL-osoite on palvelimen osoite ja siihen viittaava dokumentti on etusivu.html.
+        get("/", (req, res) -> {
+            //Luodaan kartta map.
+            HashMap map = new HashMap<>();
+            //Etusivulla on lista, jonka alkioiden attribuutin th:each arvo on "keskustelualue: ${keskustelualueet}".
+            //Liitetään avain "keskustelualueet" List<keskustelualue>-tyypin muuttujaan, joka taas palautetaan keskustelualueDaon metodilla findAll.
+            map.put("keskustelualueet", keskustelualueDao.findAll());
 
-//        get("/", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//            map.put("viesti", "tervehdys");
-//
-//            return new ModelAndView(map, "index");
-//        }, new ThymeleafTemplateEngine());
-//
-//        get("/opiskelijat", (req, res) -> {
-//            HashMap map = new HashMap<>();
+            return new ModelAndView(map, "etusivu");//Tämä rivi määrää, mitä html-sivua käytetään etusivuna. Tässä etusivu.html.
+        }, new ThymeleafTemplateEngine());
+
+        get("/opiskelijat", (req, res) -> {
+            HashMap map = new HashMap<>();
 //            map.put("opiskelijat", opiskelijaDao.findAll());
-//
-//            return new ModelAndView(map, "opiskelijat");
-//        }, new ThymeleafTemplateEngine());
-//
-//        get("/opiskelijat/:id", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
-//
-//            return new ModelAndView(map, "opiskelija");
-//        }, new ThymeleafTemplateEngine());
+
+            return new ModelAndView(map, "opiskelijat");
+        }, new ThymeleafTemplateEngine());
+
+        get("/opiskelijat/:id", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
+
+            return new ModelAndView(map, "opiskelija");
+        }, new ThymeleafTemplateEngine());
         }
     }
 }

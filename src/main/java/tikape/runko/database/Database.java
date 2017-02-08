6f7604fd8d8e1352fce0eacb0a1670ab4e43c612
Gröhn Juho A <gjuho@da2-cs-z1-01.helsinki.fi>
2017-02-08 11:07:13 +0200

    Lisäsin Databse.javaan metodeja selittäviä kommentteja. Muokkasin sqliteLauseet-metodia.

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -7,15 +7,19 @@ import java.util.List;
 public class Database {
 
     private String databaseAddress;
+    //dataBaseAddress-merkkijonoon syötetään tietokannan URL-osoite. Tässä "jdbc:sqlite:foorumi.db".
 
+    //konstruktori:
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
     }
 
+    //getConnection hakee yhteyttä tietokannan URL-osoitteeseen.
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(databaseAddress);
     }
 
+    //Init suorittaa ne lauseet, jotka sqliteLauseet-metodi on määrännyt suoritettavaksi.
     public void init() {
         List<String> lauseet = sqliteLauseet();
 
@@ -35,14 +39,30 @@ public class Database {
         }
     }
 
+    //Määrittelee ja palauttaa listan kaikista SQL:ään kirjoitettavista lauseista.
     private List<String> sqliteLauseet() {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Platon');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
+        lista.add("CREATE TABLE Keskustelualue (alueenNimi varchar(30) PRIMARY KEY, kuvaus varchar(150));");
+        lista.add(
+            "CREATE TABLE Langat (\n" +
+            "viestiNro integer PRIMARY KEY,\n" +
+            "otsikko varchar(30),\n" +
+            "alue,\n" +
+            "aikaleima timestamp DATE DEFAULT CURRENT_TIMESTAMP,\n" +
+            "FOREIGN KEY(alue) REFERENCES Keskustelualue(alueenNimi)\n" +
+            ");"
+        );
+        lista.add(
+            "CREATE TABLE Vastaukset (\n" +
+            "viestiNro integer PRIMARY KEY,\n" +
+            "lanka,\n" +
+            "teksti varchar(300),\n" +
+            "aikaleima timestamp DATE DEFAULT CURRENT_TIMESTAMP,\n" +
+            "FOREIGN KEY (lanka) REFERENCES Langat(viestiNro)\n" +
+            ");"
+        );
 
         return lista;
     }
